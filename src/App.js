b5095156a283b18d177163b559c360c44413150b
SILVIO CAVALCANTI BONFIM <silviocavalcanti2011@gmail.com>
2023-01-18 13:51:39 -0300

    update parts

--- src/App.js
+++ src/App.js
@@ -3,17 +3,19 @@ import Part from './components/Part';
 
 function App() {
 
-  const colors = [`#${Math.floor(Math.random()*16777215).toString(16)}`,`#${Math.floor(Math.random()*16777215).toString(16)}`,`#${Math.floor(Math.random()*16777215).toString(16)}`, `#${Math.floor(Math.random()*16777215).toString(16)}`];
+  const colors = ((Hue) => [`hsl(${Hue}, 80%, 50%)`, `hsl(${Hue + 90}, 80%, 50%)`, `hsl(${Hue + 180}, 80%, 50%)`, `hsl(${Hue + 270}, 80%, 50%)`])(Math.floor(Math.random() * 360));
 
-  const parts = [[0,1,2,3], [2,3,1,0], [1,3,0,2], [2,3,0,1], [3,1,2,0], [3,2,1,0], [1,0,2,3], [1,0,3,2],[3,0,1,2]]
+  const parts = [[0, 1, 2, 3], [2, 3, 1, 0], [1, 3, 0, 2], [2, 3, 0, 1], [3, 1, 2, 0], [3, 2, 1, 0], [1, 0, 2, 3], [1, 0, 3, 2], [3, 0, 1, 2]]
 
   return (
     <div className='App'>
+      <div className='boardParts'>
         {
-          parts.map((e,i) => (
-            <Part color={e.map(ee => colors[ee])} width='30vmin' height='30vmin' key={i}/>
+          parts.map((e, i) => (
+            <Part color={e.map(ee => colors[ee])} width={`${90/5}vmin`} height={`${90/5}vmin`} key={i} />
           ))
         }
+      </div>
     </div>
   );
 }
--- src/components/Part/index.js
+++ src/components/Part/index.js
@@ -6,45 +6,76 @@ import './index.css';
 import React from "react";
 
 const Part = (props) => {
+    const processTransform = (state, action) => {
+        switch (action.mode) {
+            case `SETPOSITION`:
+                return { ...state, position: { x: action.event.clientX - state.clickPosition.x, y: action.event.clientY - state.clickPosition.y } }
+                break;
+            case `SETROTATION`:
+                return { ...state, rotate: (state.rotate + 1) % 4 }
+                break;
+            case `SETCLICKPOSITION`:
+                return { ...state, clickPosition: { x: action.event.clientX - state.position.x, y: action.event.clientY - state.position.y } }
+                break;
+            case `TAGGLESELECT`:
+                return { ...state, isSelect: 1 - state.isSelect }
+                break;
+            default:
+                break;
+        }
+    }
 
-    const [isSelect, setSelect] = React.useState(0);
-    const [rotate, setRotate] = React.useState(0);
-    const [position, setPosition] = React.useState({ x: 0, y: 0 });
-    const [ClickPosition, setClickPosition] = React.useState({ x: 0, y: 0 });
+    const [transform, dispatcherTransform] = React.useReducer(processTransform,
+        {
+            position: {
+                x: 0,
+                y: 0
+            },
+            clickPosition: {
+                x: 0,
+                y: 0
+            },
+            isSelect: 0,
+            rotate: 0
+        });
 
     const handleMove = React.useCallback((event) => {
-        (isSelect === 1) && setPosition({ x: event.clientX - ClickPosition.x, y: event.clientY - ClickPosition.y })
-    }, [ClickPosition]);
+        if (transform.isSelect) {
+            dispatcherTransform({ mode: `SETPOSITION`, event: event })
+        }
+    }, [transform.clickPosition]);
 
     React.useEffect(() => {
-        if(isSelect)
+        if (transform.isSelect)
             document.getElementById('root').addEventListener('mousemove', handleMove, true)
         else
             document.getElementById('root').removeEventListener('mousemove', handleMove, true)
 
-    }, [isSelect]);
+    }, [transform.isSelect]);
 
     return (
         <div
             style={{
                 width: props.width || '200px',
                 height: props.height || '200px',
-                transform: `translate(${position.x}px, ${position.y}px) scale(1) rotate(${rotate*90}deg)`,
-                cursor: ['grab', 'grabbing'][isSelect]
-            }}
-            onContextMenu={(e) => {
-                e.preventDefault();
-                if(isSelect)
-                    setRotate(v => (v+1)%4)
+                transform: `translate(${transform.position.x}px, ${transform.position.y}px) rotate(${transform.rotate * 90}deg)`,
+                cursor: ['grab', 'grabbing'][transform.isSelect],
+                boxShadow: [`rgba(0, 0, 0, 0.02) 0px 1px 3px 0px, rgba(27, 31, 35, 0.15) 0px 0px 0px 1px`, `rgba(0, 0, 0, 0.35) 0px 5px 15px`][transform.isSelect],
+                zIndex: transform.isSelect,
+                backgroundColor: `rgba(255, 255, 255, ${[1,0.5][transform.isSelect]})`
             }}
             className='card'
+
+            onContextMenu={(e) => {
+                e.preventDefault();
+                (transform.isSelect) && dispatcherTransform({ mode: `SETROTATION` })
+            }}
+
             onClick={(e) => {
-                console.log(e.button)
-                if (isSelect === 0)
-                    setClickPosition({ x: e.clientX - position.x, y: e.clientY - position.y })
-                setSelect(v => 1 - v);
-            }
-            }>
+                (!transform.isSelect) && dispatcherTransform({ mode: `SETCLICKPOSITION`, event: e })
+                dispatcherTransform({ mode: `TAGGLESELECT` })
+            }} 
+        >
             <svg version="1.0" xmlns="http://www.w3.org/2000/svg" width="100%" height="100%" viewBox="0 0 829.000000 829.000000" preserveAspectRatio="xMidYMid meet">
                 <RopeThree color={props.color[0]} />
                 <RopeFour color={props.color[1]} />
